773937796a9a8f73be54444e51f4b1adbcf5a8ab
Crowdbotics <team@crowdbotics.com>
2020-08-11 02:34:45 +0000

    Update models

--- kiss_me_19488/wsgi.py
+++ kiss_me_19488/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kiss_me_19488.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kiss_me_19488.settings")
 
 application = get_wsgi_application()
